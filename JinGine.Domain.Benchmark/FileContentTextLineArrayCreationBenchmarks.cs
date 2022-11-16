fc94efc69007d1d78833788cdc3adc9b2548cb0b
Angel Muñoz <angelmnz@outlook.com>
2022-11-16 21:51:30 +0100

    add benchmarks to choose string-StringSegment or char[]-ArraySegment combination

---
+++ JinGine.Domain.Benchmark/FileContentTextLineArrayCreationBenchmarks.cs
@@ -0,0 +1,33 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using Microsoft.Extensions.Primitives;
+
+namespace JinGine.Domain.Benchmark;
+
+[MemoryDiagnoser]
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+[RankColumn]
+public class FileContentTextLineArrayCreationBenchmarks
+{
+    private const int ArraySize = 10_000;
+
+    /// <summary>
+    /// Fastest with 100_000 elements
+    /// Slowest with 10_000 elements
+    /// </summary>
+    [Benchmark]
+    public void CreateStringSegmentsArray()
+    {
+        var segments = new StringSegment[ArraySize];
+    }
+
+    /// <summary>
+    /// Slowest with 100_000 elements
+    /// Fastest with 10_000 elements
+    /// </summary>
+    [Benchmark]
+    public void CreateArraySegmentsArray()
+    {
+        var segments = new ArraySegment<char>[ArraySize];
+    }
+}
---
+++ JinGine.Domain.Benchmark/Program.cs
@@ -0,0 +1,6 @@
+﻿using BenchmarkDotNet.Running;
+using JinGine.Domain.Benchmark;
+
+BenchmarkRunner.Run<FileContentBenchmarks>();
+BenchmarkRunner.Run<FileContentTextLineLoopBenchmarks>();
+BenchmarkRunner.Run<FileContentTextLineArrayCreationBenchmarks>();
